ac6fcdd19415e2ba98a3e4058a130ae5ab1db859
as.stromov <as.stromov@tensor.ru>
2019-08-11 19:30:11 +0300

    bootstrap add

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
 import ArticleList from './ArticleList/ArticleList'
 import  articles from '../fixtures'
+import VisualDisplay from './VisualDisplay/VisualDisplay'
+
 import 'bootstrap/dist/css/bootstrap.css'
+import '../styles/index.css'
 
 import '../styles/App.css';
 
@@ -12,16 +15,64 @@ state={
 
 
     render() {
-        return (
-            <div className="container">
-                <div className="jumbotron">
-                <h1 className="display-3">My React App!
-                <button className="btn" onClick={this.revert}>Revert</button>
-                </h1>
+        // return (
+        //
+        //     <div className="container">
+        //         <div className="jumbotron">
+        //         <h1 className="display-3">My React App!
+        //         <button className="btn" onClick={this.revert}>Revert</button>
+        //         </h1>
+        //         </div>
+        //         <VisualDisplay/>
+        //         {/*<ArticleList articles={this.state.reverted ? articles.slice().reverse() : articles}/>*/}
+        //     </div>
+        // );
+        const border = {border: '1px solid black'};
+        return(
+            <div className="container ">
+                <div className="row jumbotron ">
+                    <div className=" col-8 card ">
+                        <div className="row  card-body ">
+                            <div className="dataVisualisation card col-6" >
+                                realTimeVideoTranslation
+                            </div>
+
+                            <div className="dataVisualisation card col-6" >
+                                realTimeVideoTranslation
+                            </div>
+                        </div>
+                        <div className=" row  " >
+
+                            <div className="testdata card card-body">testdata</div>
+                            <div className="testdata card card-body">testdata</div>
+                            <div className="testdata card card-body">testdata</div>
+                            <div className="testdata card card-body">testdata</div>
+                            <div className="testdata card card-body">testdata</div>
+                            <div className="testdata card card-body">testdata</div>
+                            <div className="testdata card card-body">testdata</div>
+                            <div className="testdata card card-body">testdata</div>
+                            <div className="testdata card card-body">testdata</div>
+
+                        </div>
+                    </div>
+                    <div className="col-4  ">
+                        <div className="card-header">
+                            Список параметров
+                        </div>
+                        <ul className="sensorsList list-group list-group-flush ">
+                            <li className="list-group-item ">testElement</li>
+                            <li className="list-group-item">testElement</li>
+                            <li className="list-group-item">testElement</li>
+                            <li className="list-group-item">testElement</li>
+                            <li className="list-group-item">testElement</li>
+                            <li className="list-group-item">testElement</li>
+                            <li className="list-group-item">testElement</li>
+                            <li className="list-group-item">testElement</li>
+                        </ul>
+                    </div>
                 </div>
-                <ArticleList articles={this.state.reverted ? articles.slice().reverse() : articles}/>
             </div>
-        );
+        )
     }
     revert =()=> this.setState({
         reverted:!this.state.reverted
---
+++ src/components/VisualDisplay/VisualDisplay.js
@@ -0,0 +1,20 @@
+import React, {Component, PureComponent} from "react";
+
+class VisualDisplay extends Component {
+
+    render(){
+        return(
+            <div className="videoTranslationAndMap">
+                <div className="dataVisualisation">
+                    <img className="testVideo" src="../a30c642217c5759a3973f2aebbeb66b1.gif" alt=""/>
+                </div>
+                <div className="dataVisualisation">
+                    realTimeVideoTranslation
+                </div>
+
+            </div>
+        );
+    }
+
+}
+export default VisualDisplay
