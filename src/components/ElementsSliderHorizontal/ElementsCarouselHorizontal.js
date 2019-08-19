7f24b7dddcccf6fdff5caae0029d5fb2670740cb
nortren <nortren@inbox.ru>
2019-08-19 23:03:16 +0300

    position horizontal button

--- src/components/ElementsSliderHorizontal/ElementsCarouselHorizontal.js
+++ src/components/ElementsSliderHorizontal/ElementsCarouselHorizontal.js
@@ -24,6 +24,7 @@ class ElementsSliderHorizontal extends Component {
     render() {
         const style = { left: this.state.stripBg };
         return (
+            <div>
             <div  className="carousel_horizontal card-body card col-12">
                 <div  style={ style } className="carousel_horizontal_line row">
                     <div className="card  carousel_horizontal_line_element">123</div>
@@ -52,7 +53,10 @@ class ElementsSliderHorizontal extends Component {
 
 
                 </div>
-                <div className="row ">
+
+
+            </div>
+                <div className="carousel-control_position carousel_horizontal card-body  col-12 ">
                     <a class="carousel-control_left left carousel-control" href="#carousel-id" role="button" data-slide="prev">
                         <span class="carousel-control_left_glyphicon_chevron  glyphicon glyphicon-chevron-left" aria-hidden="true" onClick={e => this.carouselMoveLeft()}></span>
                     </a>
@@ -60,7 +64,6 @@ class ElementsSliderHorizontal extends Component {
                         <span class="carousel-control_right_glyphicon_chevron glyphicon glyphicon-chevron-right" aria-hidden="true" onClick={e => this.carouselMoveRight()}></span>
                     </a>
                 </div>
-
             </div>
         );
     }
