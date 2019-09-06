cf1ec693a6f0ddc45217a4903e6e0c065dd631a3
nortren <nortren@inbox.ru>
2019-09-06 22:15:14 +0300

    server stream video

--- server/server_run.js
+++ server/server_run.js
@@ -28,6 +28,36 @@ app.get('/api/customers', (req, res) => {
    res.json(customers);
 });
 
+app.get('/', (req, res) => {
 
+    var qs = require('querystring');
+    var imageData = "";
+    var myId = 1;
+   /* Запуск сервера */
+    if(req.method == "POST"){
+        var fullBody = "";
+        req.on('data', function(chunk){
+            fullBody += chunk.toString();
+        });
+        req.on('end', function(){
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            var POST = qs.parse(fullBody);
+            if(POST["p"] == "new"){ // Смена изображения
+                imageData = POST["text"];
+                myId += 1;
+                res.write(imageData);
+            }else if(POST["p"] == "ajax"){
+                if(myId > parseInt(POST["last"])){
+                    if(typeof(imageData) != "undefined"){
+                        // res.write(document.body.innerHTML = ('<img src=" + '"' + imageData + '"' + "/>');" + "\n");
+                        res.write("last_message_id = " + myId + ";");
+                    }
+                }
+            }
+            res.end();
+        });
+    }else{ /* Здесь идёт отдача всего, что пользователь просит. */}
+
+});
 
 server.listen(port, () => console.log(`server started on port ${port}`));
