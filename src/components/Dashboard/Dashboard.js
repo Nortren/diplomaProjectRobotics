ac815766dd27babc69f6d4a7b3b7eea6b044da9a
nortren <nortren@inbox.ru>
2019-12-07 18:48:14 +0300

    Оживил графики и фронтовую подгрузку данных

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -14,8 +14,8 @@ class ElementsCarouselHorizontal extends Component {
     componentDidMount(prevProps) {
         this.updateCanvasDiagrams();
     setInterval(()=> {
-        this.carouselMoveRight();
-    },1)
+        this.diagramsUpdate();
+    },1000)
     }
 
     createDiagrams(idCanvas) {
@@ -26,50 +26,53 @@ class ElementsCarouselHorizontal extends Component {
         const size = {x: 150, y: 150};
         canvas.width = size.x;
         canvas.height = size.y;
-        context.font = "30px Verdana";
+        context.font = "10px Verdana";
         context.fillStyle = "#A0A1A6";
+        context.textAlign="center"
+
+
         const s = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+            const dist = 0;
 
-
-        const sum = s.reduce(function (pv, cv) {
-            return pv + cv;
-        }, 0);
-
-        const dist = 0;
-
-        function circle(linewidth, radius, mod, test) {
+        function circle(linewidth, radius, mod, test, number) {
             const sum = test.reduce(function (pv, cv) {
                 return pv + cv;
             }, 0);
             let pos = 0;
             context.lineWidth = linewidth;
-            context.fillText(test[test.length - 1], size.x * .4, size.y * .5);
-            context.moveTo(size.x * .5 + radius, size.y * .5);
-            for (let i = 0; i < test.length; i++) {
 
-                // let color = '#21' + i + '6f3';
+            const dataArray = test ? test : s ;
+
+            context.moveTo(size.x * .5 + radius, size.y * .5);
+            for (let i = 0; i < dataArray.length; i++) {
+
                 const letters = '0123456789ABCDEF';
-                let color = '#';
-                for (let i = 0; i < 6; i++) {
-                    color += letters[Math.floor(Math.random() * 16)];
+                // #2196f3
+                let color = '#2196f';
+                for (let i = 0; i < 1; i++) {
+                    let colorWord =letters[Math.floor(Math.random() * 16)];
+                    color = '#2'+colorWord+'9'+colorWord+colorWord+colorWord;
                 }
                 context.strokeStyle = color;
 
                 // ArcStart
                 let cr = twopi * pos;
-                pos += test[i] / sum;
+
+                pos += dataArray[i] / sum;
 
                 context.beginPath();
                 context.arc(size.x * .5, size.y * .5, radius,
                     (cr + dist) - Math.PI / 2,
-                    cr - dist - Math.PI / 2 + twopi * test[i] / sum
+                    cr - dist - Math.PI / 2 + twopi * dataArray[i] / sum
                 );
                 context.stroke();
                 context.closePath();
+                context.fillText(number || 0 , size.x * .5, size.y * .5);
+                context.fillText("Значение", size.x * .5, size.y * .6);
             }
         }
 
-        circle(15, size.x * .5 - 30 * .5, -(0xff / s.length) * .5, this.state._testS);
+        circle(15, size.x * .5 - 30 * .5, -(0xff / s.length) * .5, this.state._testS,this.state.elementShift);
 
     }
 
@@ -80,43 +83,21 @@ class ElementsCarouselHorizontal extends Component {
         this.createDiagrams('test4');
     }
 
-    carouselMoveLeft() {
+    diagramsUpdate() {
         this.updateCanvasDiagrams();
-        this.state._testS.push(this.state.elementShift);
-        this.setState({elementShift: this.state.elementShift - 1, _testS: this.state._testS});
-
-    }
-
-    carouselMoveRight() {
-        this.updateCanvasDiagrams();
-        this.state._testS.push(this.state.elementShift);
+        if(this.state._testS.length < 5) {
+            this.state._testS.push(this.state.elementShift);
+        }
         this.setState({elementShift: this.state.elementShift + 1, _testS: this.state._testS});
     }
 
     render() {
-        const test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
-        const elementContainerWidth = 70;
-        const lengthArrayData = test.length * elementContainerWidth;
-
-        const style = {top: this.state.elementShift};
         return (
             <div className="DashBoard">
-                <canvas style={ style } id="test1"></canvas>
+                <canvas id="test1"></canvas>
                 <canvas id="test2"></canvas>
                 <canvas id="test3"></canvas>
                 <canvas id="test4"></canvas>
-                <div className="control_position carousel_vertical card-body">
-                    <a className="carousel-control_left left carousel-control" href="#carousel-id" role="button"
-                       data-slide="prev">
-                        <span className="carousel-control_left_glyphicon_chevron  glyphicon glyphicon-chevron-up"
-                              aria-hidden="true" onClick={e => this.carouselMoveLeft()}></span>
-                    </a>
-                    <a className=" carousel-control_right  right carousel-control" href="#carousel-id" role="button"
-                       data-slide="next">
-                        <span className="carousel-control_right_glyphicon_chevron glyphicon glyphicon-chevron-down"
-                              aria-hidden="true" onClick={e => this.carouselMoveRight(lengthArrayData)}></span>
-                    </a>
-                </div>
             </div>
         );
     }
--- src/components/ElementsCarouselVertical/ElementsCarouselVertical.js
+++ src/components/ElementsCarouselVertical/ElementsCarouselVertical.js
@@ -7,49 +7,175 @@ class ElementsCarouselVertical extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            elementShift: 0
+            elementShift: 0,
+            _testArrayGraphs_1: [],
+            _testArrayGraphs_2: [],
+            _testArrayGraphs_3: [],
+            _testArrayGraphs_4: []
         };
     }
 
-    carouselMoveDown(length) {
-    this.setState({ elementShift: this.state.elementShift-50 });
-    if(this.state.elementShift <= -length){
-        this.setState({ elementShift:0 });
+    componentDidMount(prevProps) {
+        setInterval(() => {
+            this.graphsUpdate();
+        }, 100)
+
     }
-}
-    carouselMoveUp() {
-        this.setState({ elementShift: this.state.elementShift-50 });
-        if(this.state.elementShift  <= 0){
-            this.setState({ elementShift:0 });
+
+    componentDidUpdate(prevProps) {
+
+    }
+
+    drawsGraphs(idCanvas, dataGraphs) {
+        var graficsArray = {
+            g1: dataGraphs,
+            // g2: {1: 11, 2: 10, 3: 8, 4: 6, 5: 13, 6: 12, 7: 22, 8: 18, 9: 16, 10: 15},
+            // g3: {1: 5, 2: 4, 3: 2, 4: 1, 5: 7, 6: 6, 7: 16, 8: 12, 9: 10, 10: 9},
+            // g4: {1: 3, 2: 4, 3: 8, 4: 12, 5: 15, 6: 18, 7: 21, 8: 22, 9: 25, 10: 27}
+        }
+        //цвета линий
+        const colors = ['#2196f3', '#0f0', '#00f', '#0ff'];
+
+        const canvas = document.getElementById(idCanvas);
+        const gr = canvas.getContext("2d");
+        //Тут мы узнаем текущий размер окна где распологается график чтоб отрисовать размеры canvas
+        const bodySize = document.getElementsByClassName('carousel_vertical_line_element')[0];
+        canvas.setAttribute('width', bodySize.offsetWidth);
+        canvas.setAttribute('height', bodySize.offsetHeight * 0.65);
+        const maxCount = 35 + 10;
+        const x0 = 30;
+        const y0 = 30;
+        const width = canvas.width - 200;
+        const height = canvas.height - 100;
+        const stepY = Math.round(height / bodySize.offsetHeight*10);
+        const stepX = Math.round(width / bodySize.offsetWidth*10);
+
+//         gr.beginPath();
+// //Вертикальная линия
+//         gr.moveTo(x0, y0);
+//         gr.lineTo(x0, height + y0);
+// //горизонтальная линия
+//         gr.lineTo(width + x0, height + y0);
+//
+//         let m = 0;
+//         const x_max = 10;
+// //нижняя разметка и цифры
+//         for (let i = x0; m < x_max; i += stepX) {
+//             m ++;
+//             gr.moveTo(i, height + y0);
+//             gr.lineTo(i, height + y0 + 15);
+//             gr.fillText(m, i + 3, height + y0 + 15);
+//         }
+//         gr.lineWidth = 2;
+//         gr.stroke();
+//         gr.closePath();
+//рисуются кривые
+        //TODO Сделать нормальный обход данных
+        let nr_color = 0;
+        for (let graph in graficsArray) {
+            gr.beginPath();
+
+            for (let counData in graficsArray[graph]) {
+                //Чистим графики
+                if(this.state._testArrayGraphs_1.length > 80){
+                    this.setState({_testArrayGraphs_1: []});
+                    this.setState({_testArrayGraphs_2: []});
+                    this.setState({_testArrayGraphs_3: []});
+                    this.setState({_testArrayGraphs_4: []});
+                }
+                const count = graficsArray[graph][counData];
+                const x = x0 + ((counData - 1) * stepX);
+                const y = y0 + (height - count * stepY);
+
+                if (1 == counData)
+                    gr.moveTo(x, y);
+                else
+                    gr.lineTo(x, y);
+
+                // gr.arc(x, y, 2, 0, 2 * Math.PI, false);
+                // gr.fillText(count, x-5, y-5);//текст над точками
+                // gr.fillText(count, x0 - 15, y);//текст у боковой линии
+
+            }
+
+            gr.strokeStyle = colors[nr_color]; //цвет линии
+            nr_color++;
+            gr.lineWidth = 3;//толщина линии
+            gr.stroke();
         }
     }
+
+    getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+
+    graphsUpdate() {
+        this.updateCanvasGraphs();
+        if (this.state._testArrayGraphs_1.length < 100) {
+            this.state._testArrayGraphs_1.push(this.getRandomInt(30));
+            this.state._testArrayGraphs_2.push(this.getRandomInt(50));
+            this.state._testArrayGraphs_3.push(this.getRandomInt(10));
+            this.state._testArrayGraphs_4.push(this.getRandomInt(60));
+        }
+        this.setState({_testArrayGraphs_1: this.state._testArrayGraphs_1});
+        this.setState({_testArrayGraphs_2: this.state._testArrayGraphs_2});
+        this.setState({_testArrayGraphs_3: this.state._testArrayGraphs_3});
+        this.setState({_testArrayGraphs_4: this.state._testArrayGraphs_4});
+    }
+
+    updateCanvasGraphs() {
+        this.drawsGraphs(1, this.state._testArrayGraphs_1);
+        this.drawsGraphs(2, this.state._testArrayGraphs_2);
+        this.drawsGraphs(3, this.state._testArrayGraphs_3);
+        this.drawsGraphs(4, this.state._testArrayGraphs_4);
+    }
+
+    carouselMoveDown(length) {
+        this.setState({elementShift: this.state.elementShift - 150});
+        if (this.state.elementShift <= -length) {
+            this.setState({elementShift: 0});
+        }
+    }
+
+    carouselMoveUp() {
+        this.setState({elementShift: this.state.elementShift - 150});
+        if (this.state.elementShift <= 0) {
+            this.setState({elementShift: 0});
+        }
+    }
+
     render() {
-        const style = { top: this.state.elementShift };
-        const test = [{id:1,name:'Test_1',value:1},{id:2,name:'Test_2',value:2},
-            {id:3,name:'Test_3',value:3},{id:4,name:'Test_4',value:4}];
-        const elementContainerWidth = 70;
-        const lengthArrayData = test.length*elementContainerWidth;
+        const style = {top: this.state.elementShift};
+        const test = [{id: 1, name: 'Test_1', value: 1}, {id: 2, name: 'Test_2', value: 2},
+            {id: 3, name: 'Test_3', value: 3}, {id: 4, name: 'Test_4', value: 4}];
+        const elementContainerWidth = 300;
+        const lengthArrayData = test.length * elementContainerWidth;
         return (
             <div>
-            <div className="carousel_vertical card-body card col-12 sensorsList  list-group-flush">
-                <div style={ style } className="carousel_vertical_line">
+                <div className="carousel_vertical card-body card col-12 sensorsList  list-group-flush">
+                    <div style={ style } className="carousel_vertical_line">
 
-                    {test.map(function (object) {
-                        return <div key={object.id} className="list-group-item carousel_vertical_line_element">
-                            <div className="carousel_vertical_line_element-name">{object.name}</div>
-                            <div className="carousel_vertical_line_element-value">{object.value}</div>
-                        </div>;
-                    })}
+                        {test.map(function (object) {
+                            return <div key={object.id} className="list-group-item carousel_vertical_line_element">
+                                <div className="carousel_vertical_line_element-name">{object.name}</div>
+                                <div className="carousel_vertical_line_element-value">{object.value}</div>
+                                <canvas id={object.id}></canvas>
+                            </div>;
+                        })}
 
-                </div>
+                    </div>
 
-            </div >
-                <div className="control_position carousel_vertical card-body" >
-                    <a className="carousel-control_left left carousel-control" href="#carousel-id" role="button" data-slide="prev">
-                        <span className="carousel-control_left_glyphicon_chevron  glyphicon glyphicon-chevron-up" aria-hidden="true" onClick={e => this.carouselMoveUp()}></span>
+                </div >
+                <div className="control_position carousel_vertical card-body">
+                    <a className="carousel-control_left left carousel-control" href="#carousel-id" role="button"
+                       data-slide="prev">
+                        <span className="carousel-control_left_glyphicon_chevron  glyphicon glyphicon-chevron-up"
+                              aria-hidden="true" onClick={e => this.carouselMoveUp()}></span>
                     </a>
-                    <a className=" carousel-control_right  right carousel-control" href="#carousel-id" role="button" data-slide="next">
-                        <span className="carousel-control_right_glyphicon_chevron glyphicon glyphicon-chevron-down" aria-hidden="true" onClick={e => this.carouselMoveDown(lengthArrayData)}></span>
+                    <a className=" carousel-control_right  right carousel-control" href="#carousel-id" role="button"
+                       data-slide="next">
+                        <span className="carousel-control_right_glyphicon_chevron glyphicon glyphicon-chevron-down"
+                              aria-hidden="true" onClick={e => this.carouselMoveDown(lengthArrayData)}></span>
                     </a>
                 </div>
             </div>
@@ -58,7 +184,6 @@ class ElementsCarouselVertical extends Component {
     }
 
 }
-
 
 
 export default ElementsCarouselVertical
