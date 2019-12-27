2369efc62da1fd48705e547fa6b6a0844847af1c
as.stromov <as.stromov@tensor.ru>
2019-12-27 16:55:14 +0300

    new png

--- src/components/MapOfBuilding/MapOfBuilding.tsx
+++ src/components/MapOfBuilding/MapOfBuilding.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {YMaps, Map, Placemark,Polyline} from "react-yandex-maps";
+import {YMaps, Map, Placemark, Polyline} from "react-yandex-maps";
 
 export default class MapOfBuilding extends React.Component {
     constructor(props) {
@@ -25,18 +25,42 @@ export default class MapOfBuilding extends React.Component {
      */
     createCanvas(idCanvas: number, speedDrawn: number, data: object): void {
         var img = new Image();
-        img.src = "src/images/plan.jpg";
+        img.src = "src/images/plan.png";
         this.canvas = document.getElementById('canvas_1777');
         // контекст, через который будем управлять содержимым canvas
+        const width = document.getElementsByClassName('dataVisualisation')[1].offsetWidth;
+        const height = document.getElementsByClassName('dataVisualisation')[1].offsetHeight;
+        this.setState({width: width,height:height});
         const contextCanvas = this.canvas.getContext('2d');
-        contextCanvas.drawImage(img,100,100);
+        img.onload = function () {
+            //	оператор try..catch используем для обработки ошибок, например если холст не найден
+            //	в некоторых случах было замечено ошибочный вызов исключений при получении холста
+            try {
+                //	получаем контент холста
+
+                // Рисуем изображение от точки с координатами 10, 40
+                contextCanvas.drawImage(img, 0, 0, width, height);
+                contextCanvas.beginPath();
+                contextCanvas.moveTo(30,96);
+                contextCanvas.lineTo(70,66);
+                contextCanvas.lineTo(103,76);
+                contextCanvas.lineTo(170,15);
+                contextCanvas.stroke();
+            }
+            catch (err) {
+                //	выводит необходимую ошибку
+                console.log('Ошибка');
+            }
+        }
+
+
     }
 
     render() {
 
         return (
             <div className="dataVisualisation card-body col-lg-6 col-md-12 col-12">
-                <canvas id='canvas_1777' width="150" height="150"></canvas>
+                <canvas id='canvas_1777' width={this.state.width} height={this.state.height}></canvas>
             </div>
         )
     }
